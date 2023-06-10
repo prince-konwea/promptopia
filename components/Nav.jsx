366ea7cc57b6268b62349efe2c3f40aa600993ec
prince-konwea <kokodecontee123@gmail.com>
2023-06-10 03:01:13 -0700

    creating Nav component

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
 import {useState, useEffect} from "react";
@@ -6,8 +8,36 @@ import {
 } from "next-auth/react"
 
 const Nav = () => {
+    const isUserLoggedIn = true;
   return (
-    <div>Nav</div>
+    <nav className="flex-between w-full mb-16 pt-3">
+     <Link  href="/" className="flex gap-2 flex-center">
+        <Image 
+        src="/assets/images/logo.svg"
+        alt="promptopia logo"
+        width={30}
+        height={30}
+        className="object-contain"
+        />
+        <p className="logo_text">Promptopia</p>
+     </Link>
+     {/* mobile navigation */}
+     <div className="sm:flex hidden">
+       {
+        isUserLoggedIn ? (
+            <div className="flex gap-3 md:gap-3">
+                <Link href="/create-prompt" className="black_btn">
+                  Create Post
+                </Link>
+            </div>
+        ): (
+            <>
+
+            </>
+        )
+       }
+     </div>
+    </nav>
   )
 }
 
